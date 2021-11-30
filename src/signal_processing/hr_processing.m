55291f36c1a54e14651ac6f06a9b67e2757fae93
robertviramontes <rviramontes24@gmail.com>
2021-11-29 22:47:44 -0600

    Update matlab infrastructure and add codegen settings.

--- src/signal_processing/hr_processing.m
+++ src/signal_processing/hr_processing.m
@@ -2,12 +2,10 @@ function [heart_rate] = hr_processing(ppg_data, mic_data)
 %HR_PROCESSING Implements the DSP block to determine the heartrate
 %   Takes in PPG data and MEMS microphone data and 
 %   merges the processed signals to predict the heart rate.
-hold off;
-plot(ppg_data)
-hold on;
-plot(mic_data)
-hold off;
+% find peaks
+[pks, locs] = findpeaks(ppg_data);
+
 % return a heart rate 
-heart_rate = round(100*rand(1))+60;
+heart_rate = 60;
 end
 
